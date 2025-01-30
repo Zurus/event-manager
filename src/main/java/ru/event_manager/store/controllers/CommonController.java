93ac9e5e925831151fa1bc4a53fd359ed6919e05
Artur <Zulkund@mail.ru>
2025-01-30 17:13:38 +0500

    params auth fixes

--- src/main/java/ru/event_manager/store/controllers/CommonController.java
+++ src/main/java/ru/event_manager/store/controllers/CommonController.java
@@ -1,27 +1,31 @@
 package ru.event_manager.store.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Map;
+
 @Controller
 public class CommonController {
+
+    private static final Logger logger = LoggerFactory.getLogger(CommonController.class); // Логгер
+
     @GetMapping("/")
     public String index() {
         return "index"; // Страница с виджетом
     }
 
     @RequestMapping("/auth")
-    public String auth(@RequestParam("id") String id,
-                       @RequestParam("first_name") String firstName,
-                       @RequestParam("last_name") String lastName,
-                       @RequestParam("username") String username,
-                       @RequestParam("photo_url") String photoUrl,
-                       @RequestParam("auth_date") String authDate,
-                       @RequestParam("hash") String hash) {
+    public String auth(@RequestParam Map<String, String> params,
+                       Model model) {
+        params.forEach((s, s2) -> logger.debug("key {} value {}", s, s2));
         // Здесь вы можете проверить хэш и аутентифицировать пользователя
         // Например, проверка на наличие пользователя в базе данных
 
